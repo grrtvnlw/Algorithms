db67734d405c19c3780bc3f56f1ed8a04d65d244
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-04-10 12:40:30 -0400

    Add Digit Frequency solution

---
+++ digit_frequency.py
@@ -0,0 +1,15 @@
+def has_same_digit_frequency(a, b):
+    if len(a) != len(b):
+        return False
+    else:
+        a.sort, b.sort
+        if a != b:
+            return False
+        else: 
+            return True
+
+    
+
+print(has_same_digit_frequency([1, 2, 3, 4], [1, 2, 3, 5]))
+print(has_same_digit_frequency([1, 1, 1, 2, 3], [1, 2, 3]))
+print(has_same_digit_frequency([1, 2, 3], [1, 2, 3]))
