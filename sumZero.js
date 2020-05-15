632e716157b1f328cab2184a60d39b1f6871abc0
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-15 11:52:37 -0400

    solved Sum Zero algorithm

--- sumZero.js
+++ sumZero.js
@@ -1,6 +1,16 @@
 function sumZero(sortedArr) {
-  
-  return newArr;
-}
-let sortedArr = [-3, -2, -1, 0, 3, 4, 4, 5]; // [-3, 3]
-console.log()
+  let newArr = [];
+
+  for (let i = 0; i < sortedArr.length; i++) {
+    let base = sortedArr[i];
+    for (let n = 1; n < sortedArr.length; n++) {
+      if (base + sortedArr[n] === 0) {
+        newArr.push(base);
+        newArr.push(sortedArr[n]);
+        return newArr;
+      }; 
+    };
+  };
+};
+let sortedArr = [-4, -3, -1, 0, 3, 4, 4, 5]; // [-3, 3]
+console.log(sumZero(sortedArr));
