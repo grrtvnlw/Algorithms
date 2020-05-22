1753c215cbd93ea2a4fff621acc9b9bfe442ac79
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-22 12:08:49 -0400

    Stocks Exercise 2 complete

--- stocks_pt2.js
+++ stocks_pt2.js
@@ -1,17 +1,19 @@
 function stocks(prices) {
   let maxProfit = 0;
-  let currentMin = prices[0];
-  for (let price of prices) {
-    if (price < currentMin) {
-      currentMin = price;
-    } else if (price - currentMin > maxProfit) {
-      maxProfit = price - currentMin;
+  let buy = prices[0];
+  for (let sell of prices) {
+    if (sell < buy) {
+      buy = sell;
+    } else if (sell - buy > maxProfit) {
+      maxProfit = sell - buy;
     }
   }
   return maxProfit;
 }
 
 // let arr = [7,1,5,3,6,4];
-let arr = [7,6,4,3,1];
+// let arr = [7,6,4,3,1];
+let arr = [7,1,5,3,6,4];
+// let arr = [1,2,3,4,5];
 
 console.log(stocks(arr));
