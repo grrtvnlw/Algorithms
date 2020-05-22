60d2059fac6715234abd7e2ca3624143310a0040
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-05-22 11:13:00 -0400

    Completed stocks algorithm part 1

---
+++ stocks.js
@@ -0,0 +1,24 @@
+function stocks(arr) {
+  let counter = 0;
+  let difference = 0;
+  let buy = 0
+  let sell = 0
+  for (let i = 0; i < arr.length; i++) {
+    for (let n = 1; n < arr.length; n++) {
+      if (n > i) {
+        difference = arr[n] - arr[i];
+        if (difference > counter) {
+          counter = difference;
+          buy = arr[i] + 1;
+          sell = arr[n] - 1;
+        } 
+      }
+    }
+  }
+  return {profit: counter, buy: buy, sell: sell};
+}
+
+let arr = [7,1,5,3,6,4];
+// let arr = [7,6,4,3,1];
+
+console.log(stocks(arr));
