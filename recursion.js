740e6a155e412994469fffb16bbd8c5e5279ac63
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-19 10:50:26 -0400

    Added recursive solution for summing a range

--- recursion.js
+++ recursion.js
@@ -2,10 +2,19 @@
 function countdown(inp) {
   console.log(inp);
 
-  if (inp === 1) return;
+  if (inp === 1) return 1;
 
   inp -= countdown(inp - 1);
 }
 
 countdown(7);
 countdown(11);
+
+// Recursive Sum
+function sumRange(inp) {
+  if (inp === 1) return 1;
+
+  return inp + sumRange(inp - 1)
+}
+
+console.log(sumRange(6));
