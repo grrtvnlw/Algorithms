bcb2cfe75471c920536be4fe0231a4f13fc8e438
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-06-19 10:56:31 -0400

    Added recursive solution for finding a factorial

--- recursion.js
+++ recursion.js
@@ -14,7 +14,17 @@ countdown(11);
 function sumRange(inp) {
   if (inp === 1) return 1;
 
-  return inp + sumRange(inp - 1)
+  return inp + sumRange(inp - 1);
 }
 
 console.log(sumRange(6));
+
+// Recursive Factorial
+function factorial(inp) {
+  if (inp === 1) return 1;
+  
+  return inp * factorial(inp - 1);
+}
+
+console.log(factorial(5));
+
