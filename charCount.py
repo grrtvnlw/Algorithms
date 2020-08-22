e70c8a7dbfd3d6cedbe68a690ae20971d82b03cb
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-22 10:58:23 -0400

    Refactor

--- charCount.py
+++ charCount.py
@@ -1,16 +1,3 @@
-# def charCount(inp):
-#   result = {}
-
-#   for n in inp:
-#     if n in result.keys():
-#       result[n] += 1
-#     else:
-#       result[n] = 1
-
-#   return result
-
-# print(charCount('Hi there!'))
-
 def charCount(inp):
   result = {}
   inp = inp.lower()
