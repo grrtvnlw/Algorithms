2f72852a33af1eb8039fa4eb6032b97ba540862e
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-08-27 20:02:17 -0400

    refactored for an edge case

--- has_same_digit_frequency_alt.py
+++ has_same_digit_frequency_alt.py
@@ -11,13 +11,19 @@ def has_same_digit_frequency(a, b):
       result[num] = 1
 
   for num in b:
-    if num not in result.keys():
+    try:
+      if result[num] == 0:
+        print(result)
+        return False
+      else:
+        result[num] -= 1
+    except KeyError:
+      print(result)
       return False
-    else:
-      result[num] -= 1
 
+  print(result)
   return True
 
 print(has_same_digit_frequency([1, 2, 3, 4], [1, 2, 3, 5])) # False
-print(has_same_digit_frequency([1, 1, 1, 2, 3], [1, 2, 3])) # False
+print(has_same_digit_frequency([1, 1, 1, 2, 3], [1, 2, 2, 2, 3])) # False
 print(has_same_digit_frequency([1, 2, 3], [3, 2, 1])) # True
