9c49d8921b1d41cb4a0fc1df3fc5c0f2bac7da34
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-09-08 21:17:44 -0400

    Added solution for linear search

---
+++ linearSearch.js
@@ -0,0 +1,9 @@
+// Solution for Linear Search
+function linearSearch(array, value) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === value) {
+      return i;
+    }
+  }
+  return -1
+}
