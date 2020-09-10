ae585ad841a1e35f9c095b88ce16bcb5392b46a3
Gerrit Van Leeuwen <grrtvnlw@gmail.com>
2020-09-09 22:20:19 -0400

    Solutions for Bubble Sort

---
+++ bubbleSort.js
@@ -0,0 +1,36 @@
+// Multiple solutions for Bubble Sort
+function bubbleSort(arr) {
+  let noSwaps;
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1)
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
+
+function bubbleSort(arr) {
+  let noSwaps;
+  for (let i = arr.length; i > 0; i--) {
+    noSwaps = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr;
+}
